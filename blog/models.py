bce10bff8065c08c0cc039ff2578402ac964d3b0
NuxiiGit <nuxii09@gmail.com>
2020-06-11 21:01:52 +0100

    Added skill model.

--- blog/models.py
+++ blog/models.py
@@ -25,4 +25,14 @@ class Contact(models.Model):
 		self.save()
 
 	def __str__(self):
-		return self.text
+		return self.email
+
+class Skill(models.Model):
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+
+	def publish(self):
+		self.save()
+
+	def __str__(self):
+		return self.title
