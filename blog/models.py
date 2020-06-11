3e532b3eb0c216efbafc5f7ca2aaf4c292745c10
NuxiiGit <nuxii09@gmail.com>
2020-06-11 21:00:04 +0100

    Added custom contact field.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.core import validators
 
 class Post(models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -15,3 +16,13 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+class Contact(models.Model):
+	email = models.EmailField(max_length=70)
+	phone = models.CharField(max_length=16, validators=[validators.RegexValidator(r'\+(\d+)')])
+
+	def publish(self):
+		self.save()
+
+	def __str__(self):
+		return self.text
