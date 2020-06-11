b8a2c2652a4310159c37655cc80bdab1ef6d97d9
NuxiiGit <nuxii09@gmail.com>
2020-06-11 21:47:28 +0100

    Added education model.

--- cv/models.py
+++ cv/models.py
@@ -35,3 +35,18 @@ class Experience(models.Model):
 
 	def __str__(self):
 		return self.position
+
+class Education(models.Model):
+	start_date = models.DateTimeField(blank=True, null=True)
+	start_end = models.DateTimeField(blank=True, null=True)
+	degree = models.CharField(max_length=200)
+	subject = models.CharField(max_length=200)
+	location = models.CharField(max_length=200)
+	city = models.CharField(max_length=200)
+	country = models.CharField(max_length=200)
+
+	def publish(self):
+		self.save()
+
+	def __str__(self):
+		return self.subject
