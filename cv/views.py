c1cfc10696296d9077258c63c17676b84e9cd124
NuxiiGit <nuxii09@gmail.com>
2020-06-12 22:48:18 +0100

    Changed CV style.

--- cv/views.py
+++ cv/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from .models import Contact, Skill, Experience, Education
 
 def display_cv(request):
-	return render(request, 'cv/base.html', { })
+	return render(request, 'cv/display_cv.html', { })
