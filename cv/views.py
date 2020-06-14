a595b0b6f93295825b5965869975814461d25089
NuxiiGit <nuxii09@gmail.com>
2020-06-14 20:31:17 +0100

    Updated the views to order posts by most-recent to least-recent.

--- cv/views.py
+++ cv/views.py
@@ -4,8 +4,8 @@ from .models import Contact, Skill, Experience, Education
 def display_cv(request):
 	contacts = Contact.objects.all()
 	skills = Skill.objects.all()
-	experiences = Experience.objects.all().order_by('end_date')
-	educations = Experience.objects.all().order_by('end_date')
+	experiences = Experience.objects.all().order_by('-end_date')
+	educations = Experience.objects.all().order_by('-end_date')
 	return render(request, 'cv/display_cv.html', {
 		'contacts' : contacts,
 		'skills' : skills,
