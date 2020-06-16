244dd67ee87b6c8a363b2e704b5de566e57c0c86
NuxiiGit <nuxii09@gmail.com>
2020-06-17 00:35:06 +0100

    Worked on setting up a page for functional tests.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,20 @@
 from selenium import webdriver
+
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.utils import timezone
+
+from blog.models import Post
 
 import time
 
 class ExampleTestCase(StaticLiveServerTestCase):
 	
+	POST_TITLE = 'Test'
+	POST_TEXT = 'Hello world.'
+
 	def setUp(self):
 		self.browser = webdriver.Chrome('functional_tests/chromedriver.exe')
+		Post.objects.create(title=self.POST_TITLE, text=self.POST_TEXT, published_date=timezone.now())
 	
 	def tearDown(self):
 		self.browser.close()
