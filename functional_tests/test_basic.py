8496ed9174d9edb543b777472e59db4dd4fcbfdb
NuxiiGit <nuxii09@gmail.com>
2020-06-16 19:47:49 +0100

    Updated functional test to load a web page.

--- functional_tests/test_basic.py
+++ functional_tests/test_basic.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
+import time
+
 class ExampleTestCase(StaticLiveServerTestCase):
+	def setUp(self):
+		self.browser = webdriver.Chrome('functional_tests/chromedriver.exe')
+	
+	def tearDown(self):
+		self.browser.close()
+
 	def test_example(self):
-		self.assertEquals(0, 1)
+		self.browser.get(self.live_server_url)
+		self.assertEquals(1, 0)
