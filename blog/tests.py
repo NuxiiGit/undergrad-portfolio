a2e49d367402f4485fe5ae4a976774cb9f6f8d59
NuxiiGit <nuxii09@gmail.com>
2020-06-16 23:20:15 +0100

    Added a sketch for adding items to the database in tests.

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,8 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.utils import timezone
+
 from .models import Post
 from .views import post_list, post_detail
 
@@ -11,8 +13,10 @@ class BlogViewTestCase(TestCase):
 		self.assertEqual(found.func, post_list)
 
 	def test_blog_returns_html(self):
+		Post.objects.create(title='Sample title', text='Test', published_date=timezone.now())
 		request = HttpRequest()
 		response = post_list(request)
 		html = response.content.decode('utf8').strip()
 		self.assertTrue(html.startswith('<html>'))
 		self.assertTrue(html.endswith('</html>'))
+		print(html)
