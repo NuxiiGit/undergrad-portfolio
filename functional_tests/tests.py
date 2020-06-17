5fbe3517152d2cea0aaecad2363b80243cdffff8
NuxiiGit <nuxii09@gmail.com>
2020-06-17 22:46:51 +0100

    Added tag to cv link so it can be identified easily.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -24,3 +24,18 @@ class BlogTestCase(StaticLiveServerTestCase):
 		post = self.browser.find_element_by_id(self.POST_TITLE)
 		body = post.find_element_by_class_name('body')
 		self.assertEqual(body.text, self.POST_TEXT)
+
+class CvTestCase(StaticLiveServerTestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Chrome('functional_tests/chromedriver.exe')
+		
+	
+	def tearDown(self):
+		self.browser.close()
+
+	def test_homepage_correctly_displays_blog_post(self):
+		self.browser.get(self.live_server_url)
+		post = self.browser.find_element_by_id(self.POST_TITLE)
+		body = post.find_element_by_class_name('body')
+		self.assertEqual(body.text, self.POST_TEXT)
