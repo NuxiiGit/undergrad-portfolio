f4a66e09ce80d13d083c97cf8a63c6ed668f71b6
NuxiiGit <nuxii09@gmail.com>
2020-06-17 22:52:00 +0100

    Updated functional test to reduce code duplication.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -14,28 +14,14 @@ class BlogTestCase(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Chrome('functional_tests/chromedriver.exe')
+		self.URL = self.live_server_url
 		Post.objects.create(title=self.POST_TITLE, text=self.POST_TEXT, published_date=timezone.now())
 	
 	def tearDown(self):
 		self.browser.close()
 
 	def test_homepage_correctly_displays_blog_post(self):
-		self.browser.get(self.live_server_url)
-		post = self.browser.find_element_by_id(self.POST_TITLE)
-		body = post.find_element_by_class_name('body')
-		self.assertEqual(body.text, self.POST_TEXT)
-
-class CvTestCase(StaticLiveServerTestCase):
-	
-	def setUp(self):
-		self.browser = webdriver.Chrome('functional_tests/chromedriver.exe')
-		
-	
-	def tearDown(self):
-		self.browser.close()
-
-	def test_homepage_correctly_displays_blog_post(self):
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.URL)
 		post = self.browser.find_element_by_id(self.POST_TITLE)
 		body = post.find_element_by_class_name('body')
 		self.assertEqual(body.text, self.POST_TEXT)
