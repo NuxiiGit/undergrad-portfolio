ca5e4c97df2742ea91f69446291c971b89144257
NuxiiGit <nuxii09@gmail.com>
2020-06-18 01:00:44 +0100

    Made the name test pass.

--- cv/views.py
+++ cv/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import Contact, Skill, Experience, Education
+from .models import Name, Contact, Skill, Experience, Education
 
 def display_cv(request):
+	names = Name.objects.all()
 	contacts = Contact.objects.all()
 	skills = Skill.objects.all()
 	experiences = Experience.objects.all().order_by('-end_date')
 	educations = Experience.objects.all().order_by('-end_date')
 	return render(request, 'cv/display_cv.html', {
+		'names' : names,
 		'contacts' : contacts,
 		'skills' : skills,
 		'experiences' : experiences,
