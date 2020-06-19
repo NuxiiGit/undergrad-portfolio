53bde1cae36f6e6c0cd40a031e6334671c1a6e29
NuxiiGit <nuxii09@gmail.com>
2020-06-19 23:24:00 +0100

    Updated site to satisfy the work experience test.

--- cv/views.py
+++ cv/views.py
@@ -6,7 +6,7 @@ def display_cv(request):
 	contacts = Contact.objects.all()
 	skills = Skill.objects.all()
 	experiences = Experience.objects.all().order_by('-end_date')
-	educations = Experience.objects.all().order_by('-end_date')
+	educations = Education.objects.all().order_by('-end_date')
 	return render(request, 'cv/display_cv.html', {
 		'names' : names,
 		'contacts' : contacts,
