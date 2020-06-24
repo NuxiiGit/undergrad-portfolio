1103a29674d4264cfc1f4d832fff7e4e00cd2b24
NuxiiGit <nuxii09@gmail.com>
2020-06-24 18:45:32 +0100

    Updated cv views.

--- cv/views.py
+++ cv/views.py
@@ -5,8 +5,8 @@ def display_cv(request):
 	names = Name.objects.all()
 	contacts = Contact.objects.all()
 	skills = Skill.objects.all()
-	experiences = Experience.objects.all().order_by('-end_date')
-	educations = Education.objects.all().order_by('-end_date')
+	experiences = Experience.objects.all().order_by('-start_date')
+	educations = Education.objects.all().order_by('-start_date')
 	return render(request, 'cv/display_cv.html', {
 		'names' : names,
 		'contacts' : contacts,
